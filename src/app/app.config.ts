8ca41ab9dcc9f6485afdd9b041a1b524d8d97bbf
Albert Aguilera <w118676@adgbs.com>
2025-06-02 08:54:06 +0200

    add angular pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 /* factory para /assets/i18n/{lang}.json */
 export function httpLoaderFactory(http: HttpClient) {
@@ -15,15 +16,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
+    importProvidersFrom(TranslateModule.forRoot({
         defaultLanguage: 'es',
         loader: {
-          provide: TranslateLoader,
-          useFactory: httpLoaderFactory,
-          deps: [HttpClient]
+            provide: TranslateLoader,
+            useFactory: httpLoaderFactory,
+            deps: [HttpClient]
         }
-      })
-    ),
-  ]
+    })),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
